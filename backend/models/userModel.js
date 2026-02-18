6cfea859dfb038e1101ecff39cb209d2ac92d2ad
thenielthevis <daniel.davis@tup.edu.ph>
2026-02-18 22:29:44 +0800

    (BACKEND) remove enum others on basic info sex

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -49,7 +49,7 @@ const userSchema = new mongoose.Schema({
     },
     gender: {
         type: String,
-        enum: ['male', 'female', 'other'],
+        enum: ['male', 'female'],
     },
     // Physical Metrics
     physicalMetrics: {
